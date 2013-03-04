84235c02ba575b6dfa6d16807c4526da2bf8cfd1
Alejandro Mostajo <amostajo@amsgames.com>
2013-03-04 13:42:04 -0600

    Fix navbar.
    Navbar correction and css addition.

--- servicios.php
+++ servicios.php
@@ -17,18 +17,17 @@ include 'include/page_block/application.php';
 // Includes all custom CSS and BOOTSTRAP
 include 'include/page_block/html_header.php';
 ?>
-          <div class="container">
-             <ul class="nav nav-pills nav-stacked span 4" >
-             	<div class="services-custom ">
-	            <li><b>    Servicios</b></li>
-	             </div>
-                <li><a href="#">Limpieza de Vidrios</a></li>
-                <li><a href="#">Limpieza de fin de obra</a></li>
-                <li><a href="#">Limpieza de mantenimiento</a></li>
-                <li><a href="#">Limpieza de techos</a></li>
-             </ul>
-          </div>
-<!--BODY-->
+
+<div class="container">
+  <div class="span3">
+    <ul class="nav nav-tabs nav-stacked" >
+      <li><a href="#"><i class="icon-chevron-right"></i> Limpieza de Vidrios</a></li>
+      <li><a href="#"><i class="icon-chevron-right"></i> Limpieza de fin de obra</a></li>
+      <li><a href="#"><i class="icon-chevron-right"></i> Limpieza de mantenimiento</a></li>
+      <li><a href="#"><i class="icon-chevron-right"></i> Limpieza de techos</a></li>
+    </ul>
+  </div>
+</div>
 
 <?php 
 // Includes all custom CSS and BOOTSTRAP
