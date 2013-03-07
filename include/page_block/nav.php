f27c1b0032f1c7c54919e152432ec1f1b10f135a
Alejandro Mostajo <amostajo@amsgames.com>
2013-03-06 23:13:21 -0600

    Fixed nav ident.
    Ident fixed.

--- include/page_block/nav.php
+++ include/page_block/nav.php
@@ -17,27 +17,24 @@
       <div class="nav-section">
         <ul class="nav nav-pills nav-ul-margin">
           <li class="nav-option"><a href="#"><i class="icon-home"></i> Inicio</a>  </li>           
-           <li class="dropdown">
-             <a class="dropdown-toggle"
-             data-toggle="dropdown"
-             href="#">
-             <b><i class="icon-th-list"></i> Servicios </b>
-             <b class="caret"></b>
-             </a>
-                <ul class="dropdown-menu" >
-                <li><a href="#">Limpieza de mantenimiento</a></li>
-                <li><a href="#">Limpieza de fin de obra</a></li>
-                <li><a href="#">Limpieza de vidrios/fachada</a></li>
-                <li><a href="#">Limpieza de Industrias</a></li>
-                </ul>
-           </li>
+          <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+              <b><i class="icon-th-list"></i> Servicios </b><b class="caret"></b>
+            </a>
+            <ul class="dropdown-menu" >
+              <li><a href="#">Limpieza de mantenimiento</a></li>
+              <li><a href="#">Limpieza de fin de obra</a></li>
+              <li><a href="#">Limpieza de vidrios/fachada</a></li>
+              <li><a href="#">Limpieza de Industrias</a></li>
+            </ul>
+          </li>
           <li class="nav-option"><a href="#"><i class="icon-resize-small"></i> Empleo</a></li>
         </ul>
       </div>
-     </div>
-          <div class="socialnet-icons">
-          <a href="#" class="icon-chevron-right" ><img src="img/facebook.png" ></a>
-          <a href="#" class="icon-chevron-right social-margin" ><img src="img/rss.png" ></a>
-          </div>
+    </div>
+    <div class="socialnet-icons">
+      <a href="#" class="icon-chevron-right" ><img src="img/facebook.png" ></a>
+      <a href="#" class="icon-chevron-right social-margin" ><img src="img/rss.png" ></a>
+    </div>
   </nav>
 </div>
