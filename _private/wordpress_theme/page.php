6330027c8c87ce30c1b0d36380b6caf940f2a69c
Alejandro Mostajo <amostajo@amsgames.com>
2013-03-11 15:53:11 -0600

    First try, wordpress template.
    Trying to create a wordpress theme.

---
+++ _private/wordpress_theme/page.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: Global Serviclean
+
+Page.
+*/
+?>
+
+<body <?php body_class($class); ?>>
+  <?php get_header(); ?>
+
+  <div class="body-content">
+
+  	<div class="container">  		
+  	</div>
+
+  </div>
+
+  <?php get_footer(); ?>
+</body>
